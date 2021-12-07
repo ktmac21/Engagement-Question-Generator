a003d327f1ccfbdc21eefd30601013899c42c294
Katie Eagles <kteagles21@gmail.com>
2021-12-07 16:07:46 -0500

    input field clears now on submit

--- client/src/components/AddQuestionForm.js
+++ client/src/components/AddQuestionForm.js
@@ -1,39 +1,43 @@
 import { useState } from 'react';
-import "bootstrap/dist/css/bootstrap.min.css"
 import React from 'react'
 import { axios } from "../axios"
-import { Form, Button } from 'react-bootstrap';
+
 
 const AddQuestionForm = () => {
-  const [addQuestion, setAddQuestion] = useState({ question: "" });
+  const [enteredQuestion, setEnteredQuestion] = useState({
+    question: ''
+  });
   
 
   const handleChange = (e) => {
-    setAddQuestion({ question: e.target.value });
+    setEnteredQuestion({ question: e.target.value });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const newQuestion = { question: addQuestion.question };
+    const newQuestion = { question: enteredQuestion.question };
     await axios.post("/questions/add", newQuestion).catch((error) => console.log(error))
     
 
     console.log("New Question:", newQuestion);
+
+    setEnteredQuestion({ question: ""})
+    
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3">
-        <Form.Label>Add an engagement question!</Form.Label>
-        <Form.Control
+    <form onSubmit={handleSubmit}>
+      <div>Add an engagement question!
+          <input 
+          value={enteredQuestion.question}
           type="text"
           placeholder="Add a question"
           onChange={handleChange}
-        />
-      </Form.Group>
-      <Button type="submit">Add question</Button>
-    </Form>
+          />
+      </div>
+      <button type="submit">Add question</button>
+    </form>
   );
 };
 
