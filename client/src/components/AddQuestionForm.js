2a25abe606de71c21f1d4e8237381324ee973654
Katie Eagles <kteagles21@gmail.com>
2021-12-10 15:24:26 -0500

    added catch block

--- client/src/components/AddQuestionForm.js
+++ client/src/components/AddQuestionForm.js
@@ -19,7 +19,6 @@ const AddQuestionForm = () => {
     const newQuestion = { question: enteredQuestion.question };
     await axios
       .post("/questions/add", newQuestion)
-      .then((response ) => console.log(response))
       .catch((error) => console.log(error));
 
     console.log("New Question:", newQuestion);
--- server/config/connection.js
+++ server/config/connection.js
@@ -6,7 +6,9 @@ mongoose.connect(process.env.MONGO_DB || 'mongodb://localhost/engagement',  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 
-});
+})
+.then(() => console.log("Database connected"))
+.catch(err => console.log( err ));
 
 
 module.exports = mongoose.connection; 
