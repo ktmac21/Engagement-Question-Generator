20561b5addeb59474e1956564278b76dc1abf474
Katie Eagles <kteagles21@gmail.com>
2021-12-10 15:34:36 -0500

    deployment

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/questions", questionsRoute);
