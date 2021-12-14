4ffaba43a64a0f84ece421d65f151faf24932fb4
Katie Eagles <kteagles21@gmail.com>
2021-12-14 13:47:23 -0500

    updated mongoose version

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose'); 
 
-mongoose.connect(process.env.MONGO_DB || 'mongodb://localhost/engagement',  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+mongoose.connect(process.env.MONGO_DB || "mongodb://localhost/engagement", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection; 
