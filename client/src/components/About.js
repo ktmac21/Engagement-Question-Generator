1a5bfc8c893303e814a16d0606d2c3e22158bd01
Katie Eagles <kteagles21@gmail.com>
2021-12-15 16:59:30 -0500

    deployment

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -11,7 +11,7 @@ const About = () => {
                 Here is a tool!
             </div>
             <div className='description'>
-                Welcome to Forced Fun. Does your company strongly encourage you to be engaged? Do you need to think of a question
+                Welcome to Forced Fun! Does your company strongly encourage you to be engaged? Do you need to think of a question
                 to ask your fellow associates but have run out of questions? Please use Forced Fun!
             </div>
         </div>
