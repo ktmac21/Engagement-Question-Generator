da2812dcb2ab02d8a8300144aae0d4fd260175e4
Katie Eagles <kteagles21@gmail.com>
2021-12-14 20:41:01 -0500

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose'); 
 
-mongoose.connect(process.env.MONGO_DB || "mongodb://localhost/engagement", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/engagement", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
