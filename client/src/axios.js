dd8a4282ae04467cba76be542ebdecdf09bbde72
Katie Eagles <kteagles21@gmail.com>
2021-12-15 16:50:53 -0500

    database configs

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,5 +1,5 @@
 import Axios from 'axios';
 
 export const axios = Axios.create({ 
-    baseURL: 'https://stark-escarpment-53173.herokuapp.com/',
+    baseURL: 'http://localhost:3000',
 });
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose'); 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/engagement", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/engagementdb", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  
 });
 
 module.exports = mongoose.connection; 
